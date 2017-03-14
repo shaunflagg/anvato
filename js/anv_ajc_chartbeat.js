16155e39febc0547de06c0a2f66a646909a0edf9
Flagg <sflagg@cmg.int>
2017-03-14 13:20:11 -0400

    add ajc player and js

---
+++ js/anv_ajc_chartbeat.js
@@ -0,0 +1,25 @@
+ /** CONFIGURATION START **/
+        var _sf_async_config={};
+        _sf_async_config.uid = 31585;
+        _sf_async_config.domain = 'ajc.com';
+        _sf_async_config.useCanonical = true;
+        // _sf_async_config.sections = 'section1,section2 ';
+        // _sf_async_config.authors = 'author1,author2 ';
+        /** CONFIGURATION END **/
+
+        (function() {
+            function loadChartbeat() {
+                window._sf_endpt = (new Date()).getTime();
+                var e = document.createElement('script');
+                e.setAttribute('language', 'javascript');
+                e.setAttribute('type', 'text/javascript');
+                e.setAttribute('src', '//static.chartbeat.com/js/chartbeat_video.js');
+                document.body.appendChild(e);
+            }
+            var oldonload = window.onload;
+            window.onload = (typeof window.onload != 'function') ?
+                loadChartbeat : function() {
+                    oldonload();
+                    loadChartbeat();
+                };
+        })();
